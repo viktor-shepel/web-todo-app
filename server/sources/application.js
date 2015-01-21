e907295b6555fccd7ec9068f2c29f0331300c2f5
viktor.shepel <shepelvictor@bigmir.net>
2015-01-22 01:43:39 +0200

    Provide meaningfull message on server launch.

--- server/sources/application.js
+++ server/sources/application.js
@@ -19,6 +19,6 @@ app.use('/api', api.todos);
 var server = app.listen(3000, function () {
   var host = server.address().address
   var port = server.address().port
-  console.log('Example app listening at http://%s:%s', host, port)
+  console.log('Server (genuine) started http://%s:%s', host, port)
 });
 
