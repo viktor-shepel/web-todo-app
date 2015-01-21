6d124c14ccc061ad9a1caee3fe10f8b0f1eaeb53
viktor.shepel <shepelvictor@bigmir.net>
2015-01-21 14:24:20 +0200

    Add timestamp fields for `todos` records.

---
+++ server/db/migrations/20150121080100-add-updated_at-to-todos.js
@@ -0,0 +1,11 @@
+var TABLE_NAME = 'todos';
+var COLUMN_NAME = 'updated_at'
+
+exports.up = function(db, callback) {
+  db.addColumn(TABLE_NAME, COLUMN_NAME, {type: 'datetime'}, callback);
+};
+
+exports.down = function(db, callback) {
+  db.removeColumn(TABLE_NAME, COLUMN_NAME, callback);
+};
+
