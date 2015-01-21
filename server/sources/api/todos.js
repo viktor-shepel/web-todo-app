daeda490a8d5f64800bee75504060a3f1f662584
viktor.shepel <shepelvictor@bigmir.net>
2015-01-22 01:30:34 +0200

    Eliminate unnecessary callback wrappers.

--- server/sources/api/todos.js
+++ server/sources/api/todos.js
@@ -1,51 +1,32 @@
 'use strict'
 
 var express = require('express');
+var logger = require('../logger.js');
 var TodosModel = require('../models').Todos;
 
 function getAllTodos(request, response, next) {
   TodosModel.all()
-  .then(function(data) {
-    response.send(data);
-  }, function(error) {
-    console.error(error);
-  });
+  .then(response.send.bind(response), logger);
 }
 
 function getTodosById(request, response, next) {
   TodosModel.find(Number(request.params.id))
-  .then(function(data) {
-    response.send(data);
-  }, function(error) {
-    console.error(error);
-  });
+  .then(response.send.bind(response), logger);
 }
 
 function createTodo(request, response, next) {
   TodosModel.create(request.body)
-  .then(function(data) {
-    response.send(data);
-  }, function(error) {
-    console.error(error);
-  });
+  .then(response.send.bind(response), logger);
 }
 
 function updateTodo(request, response, next) {
   TodosModel.update(request.body, {where: {id: Number(request.params.id)}})
-  .then(function(data) {
-    response.send(data);
-  }, function(error) {
-    console.error(error);
-  });
+  .then(response.send.bind(response), logger);
 }
 
 function deleteTodo(request, response, next) {
   TodosModel.destroy({where: {id: Number(request.params.id)}})
-  .then(function(data) {
-    response.send(204);
-  }, function(error) {
-    console.error(error);
-  });
+  .then(response.send.bind(response, 204), logger);
 }
 
 module.exports = express.Router()
